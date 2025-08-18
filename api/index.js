8ae25f58cfcd9eb384fd86839a428acf6e2a00d2
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2025-08-18 09:48:32 -0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from "express"
+import mongoose from "mongoose";
+import   dotenv from   "dotenv"
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to MongoDB!");
+})
+.catch((err)=>{
+console.log(err);
+})
 const app=express();
 const PORT=8000;
 app.listen(PORT,()=>console.log(`Server has been started at PORT:${PORT}`));
