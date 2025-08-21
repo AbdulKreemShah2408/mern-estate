ad016e81cf3d7c3ed2ed8d39deb2b046170e9f6d
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2025-08-21 03:53:05 -0700

    Add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,8 +5,10 @@ import {
   updateUserStart,
   updateUserFailure,
   updateUserSuccess,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
 } from "../redux/user/userSlice";
-
 export default function Profile() {
   const fileRef = useRef(null);
   const { currentUser,loading,error } = useSelector((state) => state.user);
@@ -20,40 +22,30 @@ export default function Profile() {
     password: "",
     avatar: currentUser?.avatar || "",
   });
-
   const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
   const UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET;
-
   const handleFileUpload = async (file) => {
     if (!file) return;
-
     const formDataUpload = new FormData();
     formDataUpload.append("file", file);
     formDataUpload.append("upload_preset", UPLOAD_PRESET);
-
     setFilePerc(0);
     setFileUploadError(false);
-
     try {
-   
       const xhr = new XMLHttpRequest();
-
       xhr.open(
         "POST",
         `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
       );
-
       xhr.upload.addEventListener("progress", (event) => {
         if (event.lengthComputable) {
           const percent = Math.round((event.loaded / event.total) * 100);
           setFilePerc(percent);
         }
       });
-
       xhr.onload = () => {
         if (xhr.status === 200) {
           const data = JSON.parse(xhr.responseText);
-
           if (data.secure_url) {
             const updatedUser = { ...currentUser, avatar: data.secure_url };
             dispatch(signInSuccess(updatedUser));
@@ -68,36 +60,29 @@ export default function Profile() {
           setFileUploadError(true);
         }
       };
-
       xhr.onerror = () => {
         console.error("Upload error");
         setFileUploadError(true);
       };
-
       xhr.send(formDataUpload);
     } catch (error) {
       console.error("Upload exception:", error);
       setFileUploadError(true);
     }
   };
-
   const handleChange = (e) => {
     setFormData((prev) => ({
       ...prev,
       [e.target.id]: e.target.value,
     }));
   };
-
   const handleUpdate = async (e) => {
     e.preventDefault();
-
     if (!currentUser?._id) {
       console.error("User ID is missing!");
       return;
     }
-
     dispatch(updateUserStart());
-
     try {
       const res = await fetch(
         `http://localhost:8000/api/user/update/${currentUser._id}`,
@@ -108,25 +93,36 @@ export default function Profile() {
           body: JSON.stringify(formData),
         }
       );
-
       const data = await res.json();
-
       if (res.status !== 200) {
         dispatch(updateUserFailure(data.message || "Update failed"));
         return;
       }
-
       dispatch(updateUserSuccess(data));
       setUpdateSuccess(true);
     } catch (err) {
       dispatch(updateUserFailure(err.message));
     }
   };
-
+  const handleDeleteUser=async()=>{
+    try {
+      dispatch(deleteUserStart());
+      const res=await fetch(`/api/user/delete/${currentUser._id}`,{
+        method:'DELETE',
+      });
+      const data=await res.json();
+      if(data.success==false){
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess());
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-
       <form className="flex flex-col gap-4" onSubmit={handleUpdate}>
         <input
           type="file"
@@ -141,7 +137,6 @@ export default function Profile() {
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         />
-
         {filePerc > 0 && filePerc < 100 && (
           <p>Uploading: {filePerc}%</p>
         )}
@@ -151,7 +146,6 @@ export default function Profile() {
         {fileUploadError && (
           <p className="text-red-600">Upload failed. Try again.</p>
         )}
-
         <input
           type="text"
           placeholder="username"
@@ -184,16 +178,15 @@ export default function Profile() {
           {loading ? 'loading...':'Update'}
         </button>
       </form>
-
       <div className="flex justify-between mt-5 text-red-600 font-semibold px-2">
-        <button type="button" className="hover:underline">
+        <button onClick={handleDeleteUser} type="button" className="hover:underline ">
           Delete account
         </button>
-        <button type="button" className="hover:underline">
+        <button type="button" className="hover:underline ">
           Sign out
         </button>
       </div>
-      <p className="text-red-700 mt-5">{error ? error:'' }</p>
+      {/* { <p className="text-red-700 mt-5">{error ? error:'' }</p> } */}
       <p className="text-green-700 mt-5">{updateSucces ? "User is updated successfully":'' }</p>
     </div>
   );
