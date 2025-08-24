0e994050ddd0c2ef49d82e6ff6d84ca1dcaec1e2
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2025-08-24 03:26:37 -0700

    add pre installion

--- api/index.js
+++ api/index.js
@@ -6,11 +6,14 @@ import authRouter from "./routes/authRoute.js";
 import listingRouter from "./routes/listingroute.js"
 import cookieParser from "cookie-parser";
 import cors from "cors"; 
+import path from 'path'
 
 dotenv.config();
 mongoose.connect(process.env.MONGO)
   .then(() => console.log("connected to MongoDB!"))
   .catch((err) => console.log(err));
+
+  const __dirname=path.resolve();
 const app = express();
 app.use(cors({
   origin: "http://localhost:5173", 
@@ -21,6 +24,10 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing",listingRouter);
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.hrml'))
+})
 
 app.use((err, req, res, next) => {
   const statusCode = Number(err.statusCode) || 500;
