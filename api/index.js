e3592daef9854cdf8b27771517c1aeead71549f5
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2025-08-24 07:42:51 -0700

    modified index.js

--- api/index.js
+++ api/index.js
@@ -3,40 +3,53 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userroute.js";
 import authRouter from "./routes/authRoute.js";
-import listingRouter from "./routes/listingroute.js"
+import listingRouter from "./routes/listingroute.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"; 
-import path from 'path'
+import cors from "cors";
+import path from "path";
 
 dotenv.config();
-mongoose.connect(process.env.MONGO)
-  .then(() => console.log("connected to MongoDB!"))
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to MongoDB!"))
   .catch((err) => console.log(err));
 
-  const __dirname=path.resolve();
+const __dirname = path.resolve();
 const app = express();
-app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true 
-}));
+
+
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"], 
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
+
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-app.use("/api/listing",listingRouter);
-app.use(express.static(path.join(__dirname,'/client/dist')));
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'client','dist','index.hrml'))
-})
+app.use("/api/listing", listingRouter);
+
+
+app.use(express.static(path.join(__dirname, "client", "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = Number(err.statusCode) || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
-    statusCode,  
-    message,    
+    statusCode,
+    message,
   });
 });
-const PORT = 8000;
-app.listen(PORT, () => console.log(`Server has been started at PORT:${PORT}`));
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
